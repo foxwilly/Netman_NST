ce51ec0a772db638100beb81b108f35919ab198a
foxwilly <willy.fernandez.ordenanza@gmail.com>
2023-12-05 13:15:27 -0700

    Final demo

--- mgmt_scripts/add_ports.sh
+++ mgmt_scripts/add_ports.sh
@@ -1,13 +1,13 @@
-sudo docker exec clab-bgp-S1 ovs-vsctl add-port ovsbr0 e1
-sudo docker exec clab-bgp-S1 ovs-vsctl add-port ovsbr0 e2
-sudo docker exec clab-bgp-S1 ovs-vsctl add-port ovsbr0 e3
-sudo docker exec clab-bgp-S1 ovs-vsctl add-port ovsbr0 e4
+sudo docker exec clab-bgp-S1 ovs-vsctl add-port ovsbr0 e1 trunks=10,20
+sudo docker exec clab-bgp-S1 ovs-vsctl add-port ovsbr0 e2 trunks=10,20
+sudo docker exec clab-bgp-S1 ovs-vsctl add-port ovsbr0 e3 tag=10
+sudo docker exec clab-bgp-S1 ovs-vsctl add-port ovsbr0 e4 tag=20
 sudo docker exec clab-bgp-S1 ovs-vsctl add-port ovsbr0 e5
 
-sudo docker exec clab-bgp-S2 ovs-vsctl add-port ovsbr0 e1
-sudo docker exec clab-bgp-S2 ovs-vsctl add-port ovsbr0 e2
-sudo docker exec clab-bgp-S2 ovs-vsctl add-port ovsbr0 e3
-sudo docker exec clab-bgp-S2 ovs-vsctl add-port ovsbr0 e4
+sudo docker exec clab-bgp-S2 ovs-vsctl add-port ovsbr0 e1 trunks=10,20
+sudo docker exec clab-bgp-S2 ovs-vsctl add-port ovsbr0 e2 trunks=10,20
+sudo docker exec clab-bgp-S2 ovs-vsctl add-port ovsbr0 e3 tag=10
+sudo docker exec clab-bgp-S2 ovs-vsctl add-port ovsbr0 e4 tag=30
 
 sudo docker exec clab-bgp-S3 ovs-vsctl add-port ovsbr0 e1
 sudo docker exec clab-bgp-S3 ovs-vsctl add-port ovsbr0 e2
